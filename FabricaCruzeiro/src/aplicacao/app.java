8fbe2967133ffd268ccd42a89d8f370418ca8b4e
Diego Pasti <diegopasti@gmail.com>
2014-05-30 11:29:25 -0300

    Atualizando a main apenas para testar o setup de commit and push do eclipse para ver se as configurações foram definidas corretamente.

--- FabricaCruzeiro/src/aplicacao/app.java
+++ FabricaCruzeiro/src/aplicacao/app.java
@@ -6,7 +6,7 @@ import utils.TipoCruzeiros;
 public class app {
 
 	public static void main(String[] args) {
-		System.out.println("Sistema > Bem vindo Construtor de Cruzeiros");
+		System.out.println("Sistema > Bem vindo");
 		Seletor.INSTANCE.SelecionarCruzeiro(TipoCruzeiros.GRANDE.toString());
 		Seletor.INSTANCE.SelecionarCruzeiro(TipoCruzeiros.MEDIO.toString());
 		Seletor.INSTANCE.SelecionarCruzeiro(TipoCruzeiros.PEQUENO.toString());
