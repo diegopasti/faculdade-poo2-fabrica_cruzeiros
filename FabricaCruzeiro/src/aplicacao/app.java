70deb66c793e2f677a13f864319099ac9115cc62
Diego Pasti <diegopasti@gmail.com>
2014-05-30 11:24:32 -0300

    Atualizando o main novamente

--- FabricaCruzeiro/src/aplicacao/app.java
+++ FabricaCruzeiro/src/aplicacao/app.java
@@ -6,7 +6,7 @@ import utils.TipoCruzeiros;
 public class app {
 
 	public static void main(String[] args) {
-		System.out.println("Iniciando o Sistema");
+		System.out.println("Sistema > Bem vindo");
 		Seletor.INSTANCE.SelecionarCruzeiro(TipoCruzeiros.GRANDE.toString());
 		Seletor.INSTANCE.SelecionarCruzeiro(TipoCruzeiros.MEDIO.toString());
 		Seletor.INSTANCE.SelecionarCruzeiro(TipoCruzeiros.PEQUENO.toString());
